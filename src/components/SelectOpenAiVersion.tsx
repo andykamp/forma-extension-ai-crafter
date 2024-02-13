30fda2edefd5f146e2cb74d8e4d9c1622b52f1ba
andykamp <akampenes@gmail.com>
2024-02-14 00:20:44 +0100

    feat: add proper api versions

--- src/components/SelectOpenAiVersion.tsx
+++ src/components/SelectOpenAiVersion.tsx
@@ -1,13 +1,19 @@
-const openAiVersions = [
-  {
-    id: 1,
-    name: "GPT-3.5 Turbo"
-  },
-  {
-    id: 2,
-    name: "GPT-4.0"
-  }
-]
+import { useEffect, useState } from "preact/hooks";
+
+type Version = {
+    Code: number;
+    Name: string;
+    Url: string;
+    ApiKey: string;
+    Max_tokens: number | null;
+    Temperature: number;
+    Frequency_penalty: number;
+    Presence_penalty: number;
+    Top_p: number;
+    Stop: string | null;
+    Messages: string[]; // Assuming Messages is an array of strings
+};
+
 export type SelectOpenAiVersionProps = {
   value: number,
   onChange: (openAiVersion: number) => void
@@ -18,6 +24,34 @@ export default function SelectOpenAiVersion(props: SelectOpenAiVersionProps) {
     value,
     onChange
   } = props
+  const [options, setOptions] = useState<Version[]>([]);
+
+  const fetchOptions = async () => {
+    try {
+      const url = `http://localhost:8080/Get_Gpt_Deployments`
+      const result = await fetch(url, {
+        method: 'GET',
+      })
+
+      if (!result.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const res = await result.json();
+      console.log('dataaaaaaaaaaaa', res);
+      setOptions(res)
+
+      // setResult(lastMsg);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+
+  useEffect(() => {
+    fetchOptions()
+  }, []);
+
   return (
     <div>
       <label htmlFor="openai-version">
@@ -27,19 +61,19 @@ export default function SelectOpenAiVersion(props: SelectOpenAiVersionProps) {
         value={value}
         onChange={(event) => {
           const value = (event as CustomEvent).detail.value
-          if(!value) return
+          if (!value) return
           onChange(+value)
         }}
       >
         {/* // Luxon uses 1-indexed months, so we need to add 1 to the value */}
-        {openAiVersions.map((version) => (
+        {options.map((version) => (
           // <option key={version.id} value={version.name}>
           //   {version.name}
           // </option>
           <weave-select-option
-            key={version.id}
-            value={version.id}>
-            {version.name}
+            key={version.Code}
+            value={version.Code}>
+            {version.Name}
           </weave-select-option>
         ))}
       </weave-select>
